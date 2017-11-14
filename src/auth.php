0395252088706ffd747ba9f8ac4b959a92cb0f6e
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-11-14 13:42:49 -0200

    Refactor architecture of bcredi graphql client

---
+++ src/auth.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace BCredi;
+
+class Auth {
+  const ENDPOINT = "/apps/auth";
+  private $app_id;
+  private $app_secret;
+  private $client;
+
+  public function __construct(\GuzzleHttp\Client $client, $app_id, $app_secret){
+    $this->client = $client;
+    $this->app_id = $app_id;
+    $this->app_secret = $app_secret;
+  }
+
+  /**
+	* Make a request for API endpoint asking for new token
+	* @return string $token
+	*/
+ 	public function getToken(){
+    $response = $this->requestToken($this->client);
+
+    return $response['data']['token'];
+	}
+
+  private function requestToken(\GuzzleHttp\Client $client){
+    $res = $client->request('POST', self::ENDPOINT, [
+      'headers' => [
+        'Accept'     => 'application/json',
+      ], 'json' => [
+        'app_id' => $this->app_id,
+        'app_secret' => $this->app_secret
+      ]
+    ]);
+
+    if($res->getStatusCode() != 200){
+      throw new \RuntimeException("Unauthorized application credentials");
+    }
+
+    return $res->getBody();
+  }
+}
